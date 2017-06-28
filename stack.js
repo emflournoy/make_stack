a982fe4c4c8f040673f005b15ac0b05d11ecc7ba
Elizabeth Flournoy <bflournoy@gmail.com>
2017-06-28 09:40:20 -0600

    completed assignment

--- stack.js
+++ stack.js
@@ -1,5 +1,32 @@
 function Stack(){
   this.items = new Array();
+  var stack = this;
+
+  return {
+    empty: function(){
+      if (stack.items.length === 0){return true;}
+      else {return false;}
+    },
+    push: function(item){
+      stack.items.push(item);
+      return item;
+    },
+    peek: ()=>{return stack.items[0]},
+    pop: function(){
+      var done = stack.items.pop();
+      return done;
+    },
+    search: function(find){
+      let index = stack.items.indexOf(find)
+        if (index<0){
+          return .1;
+        } else {
+          return index
+        }
+    }
+
+  }
+
 }
 
 module.exports = Stack;
